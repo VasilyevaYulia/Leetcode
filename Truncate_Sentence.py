9e2055901f159a9c666bc014d0c086022408bcaf
VasilyevaYulia <81489794+VasilyevaYulia@users.noreply.github.com>
2021-04-06 22:58:15 +0300

    Create Truncate_Sentence.py

---
+++ Truncate_Sentence.py
@@ -0,0 +1,15 @@
+#A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+#For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+#You are given a sentence s and an integer k. You want to truncate s such that it contains only the first k words. Return s after truncating it.
+
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        ans = ""
+        a = s.split(" ")
+        for i in range(k):
+            ans += a[i]
+            if i != k-1:
+                ans += " "
+        return ans
+
+      # return " ".join(s.split()[:k])
