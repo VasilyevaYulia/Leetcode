bdd8f771c7733568735d60b2c7fecfcb9c97bf55
VasilyevaYulia <81489794+VasilyevaYulia@users.noreply.github.com>
2021-04-09 14:09:16 +0300

    Create Robot_Return_to_Origin.py

---
+++ Robot_Return_to_Origin.py
@@ -0,0 +1,6 @@
+#There is a robot starting at position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+#The move sequence is represented by a string, and the character moves[i] represents its ith move. Valid moves are R (right), L (left), U (up), and D (down). If the robot returns to the origin after it finishes all of its moves, return true. Otherwise, return false.
+
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        return moves.count("R") == moves.count("L") and moves.count("U") == moves.count("D")
