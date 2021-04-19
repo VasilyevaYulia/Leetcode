91fd2c138b385037edf46741b435d80f04be9c17
VasilyevaYulia <81489794+VasilyevaYulia@users.noreply.github.com>
2021-04-19 23:45:52 +0300

    Create Flipping_an_Image.py

---
+++ Flipping_an_Image.py
@@ -0,0 +1,19 @@
+'''
+Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+To flip an image horizontally means that each row of the image is reversed.
+    For example, flipping [1,1,0] horizontally results in [0,1,1].
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+    For example, inverting [0,1,1] results in [1,0,0].
+'''
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        ans = []
+        for i in image:
+            ans.append(i[::-1])
+        for i in ans:
+            for j in range(len(i)):
+                if i[j] == 0:
+                    i[j] = 1
+                else: i[j] = 0
+        return ans
